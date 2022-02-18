c2cebf082df7ffa0300ff5c3fee7585bcd6817cb
John Oram <john@oram.ca>
2022-02-17 20:00:50 -0800

    demo api call on button press

--- api/server.py
+++ api/server.py
@@ -2,15 +2,39 @@
 import uvicorn
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 from starlette.responses import RedirectResponse
 from starlette.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
 async def index():
     return RedirectResponse(url="/index.html")
+
+
+class Action1Request(BaseModel):
+    msg: str
+
+
+class Action1Response(BaseModel):
+    msg: str
+
+
+@app.post("/action1")
+async def action1(request: Action1Request) -> Action1Response:
+    print(f"doing action: `{request.msg}`")
+    return Action1Response(msg=request.msg.replace("!", "?"))
 
 
 @app.get("/tirepressure")
---
+++ app/src/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import './App.css';
+import {Button} from "semantic-ui-react";
+
+function doActionOnServer(){
+    console.log("button push")
+    fetch('http://localhost:8000/action1', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+          msg: "Hey Tom!"
+      })
+    }).then(response => {
+        return response.json()
+    }).then(data => {
+        console.log("got from server: ", data)
+    })
+
+
+
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button onClick={doActionOnServer}>Press Me</Button>
+      </header>
+    </div>
+  )
+}
+
+export default App;
