2717feb1076e8eda08a9f86726ef386a4867c594
Tom Blank <tblank@ieee.org>
2023-05-09 16:40:40 -0700

    updated IP address

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,5 +1,5 @@
 export const COMMENT= "NOTE: client/src/constants.js is source file.  DO NOT EDIT constants.json.  Run makefile to update constants.json";
-export const IPADDR= "192.168.2.177";
+export const IPADDR= "10.0.0.253";
 export const PORT= "8000";
 export const BATT_VOLTS= "12";
 export const BATT_AH= "250";
